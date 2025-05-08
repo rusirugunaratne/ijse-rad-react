7e208d83934c3db2f8ee18e54cef493ecf80ca15
rusirugunaratne <info.rusiru@gmail.com>
2025-05-08 23:28:24 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="logo">MySite</div>
+                <ul className="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+            <header className="header">
+                <h1>Welcome to MySite</h1>
+                <p>Your go-to platform for awesome content.</p>
+            </header>
+        </>
+
+    );
+}
+
+export default App;
