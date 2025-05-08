6cb0eb7add286e4b2aae32fe4323f87bd98d14aa
rusirugunaratne <info.rusiru@gmail.com>
2025-05-08 23:40:42 +0530

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
+import Navbar from "./Navbar.tsx";
+
 function App() {
     return (
         <>
-            <nav className="navbar">
-                <div className="logo">MySite</div>
-                <ul className="nav-links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+            <Navbar/>
 
             <header className="header">
                 <h1>Welcome to MySite</h1>
---
+++ src/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="logo">MySite</div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
